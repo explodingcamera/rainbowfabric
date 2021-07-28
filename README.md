a3f75a8ce87c221a23c868cea9082f7035b91423
Henry Gressmann <mail@henrygressmann.de>
2021-07-28 22:46:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# RainbowFabric
+
+> Polyglot Fabric mods using GraalVM
+
+## Supported Languages
+
+* **JavaScript/TypeScript**\
+    JavaScript and TypeScript support will be the major focus for the first version.
+
+Outside of JS, support for all graalvm polyglot languages would be possible.
---
+++ core/src/main/java/com/explodingcamera/rainbowcore/RainbowCore.java
@@ -0,0 +1,18 @@
+package com.explodingcamera.rainbowcore;
+
+import net.fabricmc.api.ModInitializer;
+import org.graalvm.polyglot.*;
+import org.graalvm.polyglot.proxy.*;
+ 
+// https://github.com/grakkit/grakkit
+
+public class RainbowCore implements ModInitializer {
+    @Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		System.out.println("Yeet!");
+	}
+}
