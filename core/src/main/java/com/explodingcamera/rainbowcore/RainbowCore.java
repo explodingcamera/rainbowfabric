633f349a8c56a50429eec0152fe9d28840b90376
Henry <mail@henrygressmann.de>
2021-08-28 15:21:32 +0200

    update fabric
    Signed-off-by: Henry <mail@henrygressmann.de>

--- core/src/main/java/com/explodingcamera/rainbowcore/RainbowCore.java
+++ core/src/main/java/com/explodingcamera/rainbowcore/RainbowCore.java
@@ -3,11 +3,11 @@ package com.explodingcamera.rainbowcore;
 import net.fabricmc.api.ModInitializer;
 import org.graalvm.polyglot.*;
 import org.graalvm.polyglot.proxy.*;
- 
+
 // https://github.com/grakkit/grakkit
 
 public class RainbowCore implements ModInitializer {
-    @Override
+	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
